faca036d2e9423ce1dd9078b06b5383ee4587d8c
Isadora Moura <isadora-conceicao@hotmail.com>
2023-03-10 02:26:29 -0300

    gerar senha

---
+++ password_gen.py
@@ -0,0 +1,14 @@
+import random
+import PySimpleGUI as SyntaxWarning
+import os
+
+class PassGen:
+    def __init__(self):
+        pass
+    def Iniciar(self):
+        pass
+    def salvar_senha(self):
+        pass
+
+gen = PassGen()
+gen.Iniciar()
--- projeto2.py
+++ projeto2.py
@@ -1,2 +1,1 @@
 # projeto 2 ("Gerador de senhas Python")
-print("dificil")
