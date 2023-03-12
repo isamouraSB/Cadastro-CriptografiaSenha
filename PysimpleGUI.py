566155bdc7e448e0b0f2b7db9aa3e80ab4d0effc
Isadora Moura <isadora-conceicao@hotmail.com>
2023-03-12 02:11:39 -0300

    visual

--- PysimpleGUI.py
+++ PysimpleGUI.py
@@ -4,23 +4,41 @@ class TelaPython:
     def __init__(self):
         #Layout
         layout = [
-       [sg.Text("Cadastre-se",size=(15,0))],
-       [sg.Text("Nome",size=(10,0))],
-       [sg.Input(size=(20,0))],
-       [sg.Text("E-mail \ Login",size=(20,0))],
-       [sg.Input(size=(20,0))],
-       [sg.Text("Senha",size=(10,0))],
-       [sg.Input(size=(20,0))],
-       [sg.Button("Enviar",size=(7,0))]
+           [sg.Text("Cadastre-se",size=(15,0))],
+           [sg.Text("E-mail \ Login",size=(20,0))],
+           [sg.Input(size=(20,0),key="login")],
+           [sg.Text("Nome",size=(10,0))],
+           [sg.Input(size=(20,0),key="nome")],
+           [sg.Text("Gênero",size=(10,0))],
+           [sg.Checkbox("F",key="feminino"),sg.Checkbox("M",key="masculino"),sg.Checkbox("Prefiro não dizer",key="nulo")],
+           [sg.Text("Senha",size=(10,0))],
+           [sg.Input(size=(20,0),key="senha")],
+           [sg.Button("Enviar",size=(7,0))]
         ]
         #Janela
-        janela = sg.Window("Dados do usuario").layout(layout)
-
-        #Extrair os dados da tela
-        self.button, self.values = janela.Read() 
+        self.janela = sg.Window("Dados do usuario").layout(layout)
+        
+        
 
     def Iniciar(self):
-        print(self.values)
+        while True:
+            #Extrair os dados da tela
+            self.button, self.values = self.janela.Read()
+            login = self.values["login"]
+            nome = self.values["nome"]
+            genero_F = self.values["feminino"]
+            genero_M = self.values["masculino"]
+            genero_nulo = self.values["nulo"]
+            senha = self.values["senha"]
+            print(f"login: {login}")
+            print(f"nome: {nome}")
+            print(f"genero_F: {genero_F}")
+            print(f"genero_M: {genero_M}")
+            print(f"genero_nulo: {genero_nulo}")
+            print(f"senha: {senha}") 
+        
+
+
 
 tela = TelaPython()
 tela.Iniciar()
